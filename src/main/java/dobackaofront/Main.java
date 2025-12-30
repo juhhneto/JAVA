a08831e2bed8eb02a2200b9b0cfb5c9e07edc218
Juliana Neto <juneto.s88@gmail.com>
2025-12-30 16:13:16 -0300

    added notes

---
+++ src/main/java/dobackaofront/Main.java
@@ -0,0 +1,29 @@
+package dobackaofront;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // modificadores de acesso - public, protected, private[
+        /*
+            VARIAVEIS - tipos de dados (intm float, boolean, char, String
+            ESTRUTURAS CONDICIONAIS - if, else, else if, switch
+            ESTRUTURAS DE REPETIÇÃO - for, while, do while
+            BLOCOS DE CODIGOS(metodos)
+            ORIENTAÇÃO A OBJETOS POO
+                -classes
+                -objetos
+                -atributos
+                -metodos
+                -construtores
+                -herança
+                -polimorfismo
+                -associação
+                -interface
+                -classes e métodos abstratos(absttração)
+                -coleções
+                -pacotes
+         */
+        System.out.println("Hello World");
+    }
+}
